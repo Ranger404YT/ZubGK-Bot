c42bd9bd73a91e62220faf6e8942c1659aacebf3
Ranger404YT <92625503+Ranger404YT@users.noreply.github.com>
2022-04-16 11:21:12 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,7 @@
 settings = {
-    'token': 'OTU5MDk4NDI1MzA2MTI0MzQ4.YkW8Aw.uX-EHtcLDZ698Gp5pEjxlu1RKn8',
+    'token': 'token',
     'bot': 'ZumGk',
-    'owner': 'Ranger404#6821',
+    'owner': '',
     'id': '959098425306124348',
     'prefix': '+',
     'admin': True,
@@ -9,6 +9,4 @@ settings = {
 }
 
 if settings['admin'] == True:
-    settings['admins'] = ['GivKa#2476','Ranger404#6821','Gyutaro#0037']
-
-#URL: https://discord.com/oauth2/authorize?client_id=959098425306124348&scope=bot&permissions=8
+    settings['admins'] = ['Ranger404#6821','','']
